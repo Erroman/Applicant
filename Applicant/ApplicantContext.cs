51eade567738a7451f00367a71101f465aa09252
Vladimir Romanovsky <vladimir_roman@mail.ru>
2021-04-07 16:08:18 +0500

    Добавлен файл доя управления контекстом базы данных.

---
+++ Applicant/ApplicantContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeApplicants 
+{
+    class ApplicantContext:DbContext
+    {
+        public DbSet<Applicant> Applicants { get; set; }
+    }
+}
