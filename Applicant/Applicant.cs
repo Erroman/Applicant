ce6921eb249620cb9365d945ee9fa5c37303c7da
Vladimir Romanovsky <vladimir_roman@mail.ru>
2021-04-07 15:23:02 +0500

    Добавлен класс Applicant с данными студента

---
+++ Applicant/Applicant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeApplicants
+{
+    class Applicant
+    {
+        public int ApplicantId { get; set; }
+        public string Surname { get; set; }
+        public string GivenName { get; set; }
+        public string Patronimic { get; set; }
+        public string YearOfBirth { get; set; }
+        public string YearOfGraduation { get; set; }
+        public string ChosenSpeciality { get; set; }
+        public string CodeOfClass { get; set; }
+        public string GradeOfEGE { get; set; }
+
+
+
+    }
+}
